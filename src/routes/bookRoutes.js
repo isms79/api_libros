66c07e86284a7e45d9c46bb3da3a1986445c3e25
isms79 <isaacdavidmez79@gmail.com>
2023-09-15 08:06:54 -0500

    rutas de libros organizadas

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,11 +2,19 @@ const express = require('express');
 const router = express.Router();
 const BookController = require('../controllers/BookController');
 
-// Rutas para libros
-router.get('/', BookController.getAllBooks);
-router.get('/:id', BookController.getBookById);
-router.post('/', BookController.createBook);
-router.put('/:id', BookController.updateBook);
-router.delete('/:id', BookController.deleteBook);
+// Ruta para obtener todos los libros
+router.get('/books', BookController.getAllBooks);
+
+// Ruta para obtener un libro por ID
+router.get('/books/:id', BookController.getBookById);
+
+// Ruta para crear un nuevo libro
+router.post('/books', BookController.createBook);
+
+// Ruta para actualizar un libro por ID
+router.put('/books/:id', BookController.updateBook);
+
+// Ruta para eliminar un libro por ID
+router.delete('/books/:id', BookController.deleteBook);
 
 module.exports = router;
--- src/routes/roleRoutes.js
+++ src/routes/roleRoutes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const RoleController = require('../controllers/RoleController');
 
 // Ruta para obtener todos los roles
-router.get('/', RoleController.getAllRoles);
+router.get('/roles', RoleController.getAllRoles);
 
 // Ruta para obtener un rol por ID
-router.get('/:id', RoleController.getRoleById);
+router.get('/roles/:id', RoleController.getRoleById);
 
 // Ruta para crear un nuevo rol
-router.post('/', RoleController.createRole);
+router.post('/roles', RoleController.createRole);
 
 // Ruta para actualizar un rol por ID
-router.put('/:id', RoleController.updateRole);
+router.put('/roles/:id', RoleController.updateRole);
 
 // Ruta para eliminar un rol por ID
-router.delete('/:id', RoleController.deleteRole);
+router.delete('/roles/:id', RoleController.deleteRole);
 
 module.exports = router;
